c5762acfea7b9bc4c276e577f54b525f15384bd4
Roman Slnkv <romanxs610@gmail.com>
2022-06-13 19:17:16 +0500

    first commit project Barber

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import * as slnkvFunction from "./modules/functions.js";
+slnkvFunction.isWebp();
+import './_vendor.js';
+import vars from './_vars.js';
+import './_functions.js';
+import './_components.js';
